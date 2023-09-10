7e02732d355e9468e0abd0e0fc3c95f0bf2cff1f
Luis González <ljgonzalez@uc.cl>
2023-09-10 00:15:45 -0300

    🚧 wroking(API/src/controllers/validation.controller.js): Añade métodos de controlador para validaciones de stocks.

---
+++ API/src/controllers/validation.controller.js
@@ -0,0 +1,175 @@
+const Stock = require('../models/validation.model')
+
+const getValidations = async (req, res) => {
+  const page = Math.max(1, req.query.page) || 1
+  const size = Math.max(1, req.query.size) || 25
+  const offset = (page - 1) * size
+  
+  try {
+    const stocks = await Stock.findAll({
+      limit: size,
+      offset: offset,
+    })
+    
+    if (stocks.length > 0) {
+      res.status(200).json({stocks})
+      
+    } else {
+      res.status(404).json({message: "No validation found"})
+    }
+    
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
+
+const getValidationsByGroup = async (req, res) => {
+  const page = Math.max(1, req.query.page) || 1
+  const size = Math.min(1, req.query.size) || 25
+  const offset = (page - 1) * size
+  
+  try {
+    const { group } = req.params
+    const stock = await Stock.findAll({
+      where: {
+        group_id: group
+      },
+      limit: size,
+      offset: offset,
+    })
+    
+    if (stock.length > 0) {
+      res.status(200).json({stock})
+      
+    } else {
+      res.status(404).json({message: "No stocks found"})
+    }
+    
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
+
+const getValidationsBySeller = async (req, res) => {
+  const page = Math.max(1, req.query.page) || 1
+  const size = Math.min(1, req.query.size) || 25
+  const offset = (page - 1) * size
+  
+  try {
+    const { seller } = req.params
+    
+    const stock = await Stock.findAll({
+      where: {
+        seller: parseInt(seller)
+      },
+      limit: size,
+      offset: offset,
+    })
+    
+    if (stock.length > 0) {
+      res.status(200).json({stock})
+      
+    } else {
+      res.status(404).json({message: "No stocks found"})
+    }
+    
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
+
+const getValidationsByValid = async (req, res) => {
+  // http(s)://host:port/is_valid=<true/false>
+  const page = Math.max(1, req.query.page) || 1
+  const size = Math.min(1, req.query.size) || 25
+  const offset = (page - 1) * size
+  
+  try {
+    const { is_valid } = req.params
+    
+    if (is_valid.toLowerCase() !== 'true' || is_valid.toLowerCase() !== 'false') {
+      res.status(400).json({message: "Invalid value for is_valid. Must be 'true' or 'false'"})
+      
+    } else if (is_valid.toLowerCase() === 'true') {
+      const stock = await Stock.findAll({
+        where: {
+          valid: true
+        },
+        limit: size,
+        offset: offset,
+      })
+      
+    } else {
+      const stock = await Stock.findAll({
+        where: {
+          valid: false
+        },
+        limit: size,
+        offset: offset,
+      })
+    }
+    
+    if (stock.length > 0) {
+      res.status(200).json({stock})
+      
+    } else {
+      res.status(404).json({message: "No stocks found"})
+    }
+    
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
+
+const postStock = async (req, res) => {
+  try {
+    const { message } = req.body;
+    if (!message) {
+      return res.status(400).json({ message: "Message field is missing" });
+    }
+    
+    const data = JSON.parse(message);
+    if (!data.stocks) {
+      return res.status(400).json({ message: "Stocks field is missing" });
+    }
+    
+    const stocks = JSON.parse(data.stocks);
+    const results = [];
+    
+    for (const stock of stocks) {
+      const { symbol, shortName, price, currency, source } = stock;
+      
+      if (!symbol || !shortName || !price || !currency || !source) {
+        results.push({ message: `Missing fields for stock ${symbol || ''}` });
+        continue;
+      }
+      
+      try {
+        await Stock.create({
+          symbol,
+          shortName,
+          price,
+          currency,
+          source,
+        });
+        
+        results.push({ message: `Stock ${symbol} created at ${price}` });
+        
+      } catch (error) {
+        results.push({ message: `Error listing stock ${symbol}`, error: error.message });
+      }
+    }
+    
+    res.status(201).json({ results });
+  } catch (error) {
+    res.status(400).json({ message: "Bad Request", error: error.message });
+  }
+};
+
+
+
+module.exports = {
+  getStocks,
+  postStock,
+  getStocksByName
+}
