5b6c378f01dc36c6d0135a25cb3ee82d25f44771
Luis González <ljgonzalez@uc.cl>
2023-09-09 22:07:38 -0300

    🚑 fix(): Variables de entorno "modularizadas" +  try-catch con conexión al broker +  Typo en variable de entorno (USER -> USERNAME)

--- MQTT/src/config/mqtt.config.js
+++ MQTT/src/config/mqtt.config.js
@@ -1,13 +1,24 @@
 const mqtt = require('mqtt')
 require('dotenv').config()
 
-const url = 'mqtt://' + process.env.HOST + ':' + process.env.PORT 
+const url = 'mqtt://' + process.env.HOST + ':' + process.env.PORT
 const options = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD
 }
-const client = mqtt.connect(url, options)
 
-module.exports = { 
-  client 
+let client = null;
+
+try {
+  client = mqtt.connect(url, options)
+  console.log("connected!")
+  
+} catch (error) {
+  console.log("not connected :'(")
+  console.log(error)
+  process.exit(1)
+}
+
+module.exports = {
+  client
 }
