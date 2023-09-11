3dd54f105a70cfca518e00642ea443a4599a3654
Luis González <ljgonzalez@uc.cl>
2023-09-11 01:13:31 -0300

    ✏️ fix(MQTT/src/server.js): Corregir texto del mensaje de log al iniciar servidor.

--- MQTT/src/server.js
+++ MQTT/src/server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.MQTT_PORT, () => {
-    console.log(`▶️ | Server running on port ${process.env.MQTT_PORT}`)
+    console.log(`▶️ | Servidor MQTT Listener corriendo en puerto ${process.env.MQTT_PORT}`)
 })
 
 //// Para testear la interacción con el broke MQTT
