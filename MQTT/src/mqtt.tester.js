e9036fdf60bf40c416c5a2b04cd299c7ee3a6fc7
Luis González <ljgonzalez@uc.cl>
2023-09-11 02:13:23 -0300

    🌳 refactor(MQTT/src/mqtt.tester.js, template.env): Actualiza variable group_id con variable de entorno y elimina variables de puertos en template.env.

--- MQTT/src/mqtt.tester.js
+++ MQTT/src/mqtt.tester.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const my_dictionary = {
   request_id: '63fe8daa-4f94-11ee-be56-0242ac120002',
-  group_id: '13',
+  group_id: `${process.env.GROUP_NUMBER}`,
   symbol: "AAPL",
   datetime: new Date(),
   deposit_token: '',
