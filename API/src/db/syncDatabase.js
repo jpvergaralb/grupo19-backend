bc6a784b3e464c8025aa08a4e12dd0563f12f094
jpvergaralb <jpvergaralb@uc.cl>
2023-09-15 04:01:22 -0300

    refactor: modularize syncdb function

---
+++ API/src/db/syncDatabase.js
@@ -0,0 +1,15 @@
+const sequelize = require('./db')
+
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync({alter: true})
+    console.log('😄| Conexión a la base de datos exitosa.');
+    
+  } catch (error) {
+    console.log('😡| No se pudo conectar a la base de datos.')
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+module.exports = syncDatabase
