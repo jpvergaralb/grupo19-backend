d85e965771a0fd5f680589ae000fcb8994461316
Luis González <ljgonzalez@uc.cl>
2023-10-15 19:06:40 -0300

    ✨ feat(workers/src/environment.py): Mejora en la carga y manejo de variables de entorno.

--- workers/src/environment.py
+++ workers/src/environment.py
@@ -3,6 +3,7 @@ import dotenv
 import os
 import sys
 
+from logs import logger as log, logging, print
 from utils import convert_str_to_typed
 
 # ------------------------------------
@@ -14,27 +15,67 @@ env: dict
 # alias
 getenv = os.environ.get
 
+for path in ('.', '..', '../..', '../..'):
+    located = False
 
-if os.path.isfile('.env'):
-    # Cargo variables de entorno.
-    print("Cargando variables de entorno...")
-    dotenv.load_dotenv(".env")
-else:
-    print("Archivo .env no encontrado.\n"
-          "Usando variables preexistentes.")
+    for prefix in ('', 'example', 'template'):
+        file_path = f'{path}/{prefix}.env'
+
+        if os.path.isfile(file_path):
+            # Cargo variables de entorno.
+            log.info(f"Cargando variables de entorno de '{file_path}'...")
+            dotenv.load_dotenv(file_path)
+            located = True
+            break
+
+    if located:
+        break
+
+if not located:
+    log.warning("Archivo .env no encontrado.\n"
+                "\t\t\t\tUsando variables preexistentes.")
 
 env = {
-    "SAMPLE_VARIABLE": getenv("SAMPLE_VARIABLE"),
+    "LOG_LEVEL": getenv("LOG_LEVEL"),
 }
+
+log_level = env["LOG_LEVEL"].upper()
+
+match log_level:
+    case _ if log_level in ("TRACE", "VERBOSE", "DEBUG"):
+        log.setLevel(logging.DEBUG)
+
+    case _ if log_level in ("INFO", "INFORMATION"):
+        log.setLevel(logging.INFO)
+
+    case _ if log_level in ("WARN", "WARNING"):
+        log.setLevel(logging.WARNING)
+
+    case _ if log_level in ("ERR", "ERROR"):
+        log.setLevel(logging.ERROR)
+
+    case _ if log_level in ("FTL", "FATAL", "CRIT", "CRITICAL"):
+        log.setLevel(logging.CRITICAL)
+
+    case _ if log_level in ("NONE", "NULL", "NO", "QUIET", "SILENT"):
+        log.setLevel(logging.CRITICAL + 1)
+
+    case _:
+        log.warning(f"Valor no reconocido para LOG_LEVEL: {env['LOG_LEVEL']}. "
+                    f"Usando el nivel predeterminado INFO.")
+        env["LOG_LEVEL"] = "INFO"
+        log.setLevel(logging.INFO)
 
 # Error catching
 for env_var in env.keys():
     if str(env[env_var]) in ("None", "none", "", None):
-        print(f"Variable {env_var} no definida.")
-        print(f"Saliendo con exit code 1")
+        log.critical(f"Variable '{env_var}' no definida.\n"
+                     f"\t\t\t\tSaliendo con exit code 1")
         sys.exit(1)
 
 # Convertir a tipos de variable correspondiente
-
 for env_var in env.keys():
     env[env_var] = convert_str_to_typed(env[env_var])
+
+if __name__ == '__main__':
+    print("hello_world")
