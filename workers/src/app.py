e035fe6e767823b19a8374d8f3c11c17ca030f0b
Luis González <ljgonzalez@uc.cl>
2023-10-16 09:23:50 -0300

    Intentar convertir al tipo correcto de variable + imports

--- workers/src/app.py
+++ workers/src/app.py
@@ -1,3 +1,44 @@
 from logs import logger as log
 from logs import print
 from environment import env
+
+from pydantic import BaseModel
+from fastapi import Depends, FastAPI, Header, HTTPException, Path, Query
+from tasks import app as celery_app, redis_client
+import asyncio
+import json
+from random import randint
+from typing import List, Union
+import requests
+
+
+# ------------------------------------
+# Intentar convertir al tipo correcto de variable desde el json
+class TaskIn(BaseModel):
+    name: str
+
+
+class StocksData(BaseModel):
+    starting_date_epoch: int
+    stock_symbol: str
+
+
+class CeleryJob(BaseModel):
+    JobId: str
+    challenges: List[StocksData]
+
+# ------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
