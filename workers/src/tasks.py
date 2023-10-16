3190ec77139d497d144d0371400007b277bc9a58
Luis González <ljgonzalez@uc.cl>
2023-10-16 07:48:03 -0300

    stop grace period; Incluir nametask en task

--- workers/src/tasks.py
+++ workers/src/tasks.py
@@ -1,9 +1,17 @@
+from logs import logger as log
+from logs import print
+from environment import env
+
 from celery import Celery
+from time import sleep
 
 app = Celery('tasks',
              backend='redis://redis_workers:6379/0',
              broker='redis://redis_workers:6379/0')
 
-@app.task
+
+@app.task(name="tasks.add")
 def add(x, y):
     return x + y
+
+
