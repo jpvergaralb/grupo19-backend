3de7d3b1839fa3336cd5a8326b6fee3029aad537
Luis González <ljgonzalez@uc.cl>
2023-10-16 08:11:29 -0300

    tasks: tarea para regresión linea vacía

--- workers/src/tasks.py
+++ workers/src/tasks.py
@@ -1,17 +1,26 @@
-from logs import logger as log
-from logs import print
-from environment import env
+from random import random
+from time import sleep
 
 from celery import Celery
-from time import sleep
+from redis import Redis
 
 app = Celery('tasks',
              backend='redis://redis_workers:6379/0',
              broker='redis://redis_workers:6379/0')
+redis_client = Redis(host='redis_workers', port=6379, db=0)
 
 
+# --------------------------------------------------
+
+# Tarea de ejemplo
 @app.task(name="tasks.add")
 def add(x, y):
+    sleep(random() * 3)
     return x + y
 
+# --------------------------------------------------
+
+@app.task(name="tasks.linear_regression")
+def linear_regression():
+    pass
 
