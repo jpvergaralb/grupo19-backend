4d6030d1f4595363d4abda78d9b249c1c45389ca
Luis González <ljgonzalez@uc.cl>
2023-10-15 22:39:29 -0300

    Cambios feos.

--- workers/src/entrypoint.celery.sh
+++ workers/src/entrypoint.celery.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 python3 -m celery --app tasks worker --loglevel=INFO
-#celery --help
 echo "Hello Celery"
-#celery -A tasks worker --loglevel=INFO
--- workers/src/tasks.py
+++ workers/src/tasks.py
@@ -1,29 +1,9 @@
-from environment import env
-from logs import logger as log
-from logs import print
-
 from celery import Celery
-from redis import Redis
 
+app = Celery('tasks',
+             backend='redis://redis_workers:6379/0',
+             broker='redis://redis_workers:6379/0')
 
-# celery_broker = f"{env['REDIS_PROTOCOL']}://{env['REDIS_HOST']}:{env['REDIS_PORT']}/{env['REDIS_DATABASE']}"
-
-celery_app = Celery('tasks')
-# celery_app = Celery('tasks', broker=celery_broker, backend=celery_broker)
-# celery_app = Celery('tasks', broker=celery_broker)
-# redis_client = Redis(host=f"{env['REDIS_HOST']}", port=env["REDIS_PORT"], db=env['REDIS_DATABASE'])
-
-
-# @celery_app.task(name="task.nombre_de_la_funcion")
-# def nombre_de_la_funcion(variable):
-#     print(variable)
-#     return variable
-#
-#
-# @celery_app.task(name="task.suma_de_dos_variables")
-# def suma_de_dos_variables(variable_1: int, variable_2: int) -> int:
-#     res = variable_1 + variable_2
-#     print(f"{variable_1} + {variable_2} = {res}")
-#     return res
-
-
+@app.task
+def add(x, y):
+    return x + y
