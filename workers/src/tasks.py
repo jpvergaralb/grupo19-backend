f3fb01cced03c61c07c7c18b225f24d85456b580
Luis <ljgonzalez@uc.cl>
2023-10-23 15:53:45 -0300

    Documentación

--- workers/src/tasks.py
+++ workers/src/tasks.py
@@ -29,12 +29,63 @@ app = Celery('tasks',
 
 @app.task(name="tasks.add")
 def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    """
+    --- Documentación por ChatGPT ---
+    Tarea de Celery que suma dos números después de dormir durante un tiempo aleatorio.
+
+    Params
+    ----------
+    :param x: Union[int, float]
+        Primer número a sumar. Puede ser un entero o un decimal.
+    :param y: Union[int, float]
+        Segundo número a sumar. Puede ser un entero o un decimal.
+
+    :return: Union[int, float]
+    -------
+        Resultado de la suma de x e y.
+
+    Notas
+    -----
+    Esta tarea tiene un retraso introducido artificialmente mediante el método `sleep()`,
+    que detiene la ejecución durante un tiempo aleatorio entre 0 y 3 segundos. Esta
+    característica es útil para simular tareas que toman un tiempo variable para completarse.
+
+    Ejemplo
+    -------
+    >>> result = add.delay(5, 7)
+    >>> result.get(timeout=10)
+    12
+    """
     sleep(random() * 3)
     return x + y
 
 
 @app.task(name="tasks.find_n_prime_slowly")
 def find_prime_slowly(number: int) -> int:
+    """
+    --- Documentación por ChatGPT ---
+    Tarea de Celery que encuentra el número primo más grande menor o igual a un número dado.
+
+    Params
+    ----------
+    :param number: int
+        Número hasta el cual se busca el número primo más grande.
+
+    :return: int
+    -------
+        El número primo más grande que es menor o igual al número proporcionado.
+
+    Notas
+    -----
+    La función utiliza una implementación simple y no optimizada para determinar si un número es primo.
+    Puede ser lenta para valores grandes de `number`.
+
+    Ejemplo
+    -------
+    >>> result = find_prime_slowly.delay(20)
+    >>> result.get(timeout=10)
+    19
+    """
     def is_prime(n):
         if n <= 1:
             return False
@@ -55,6 +106,31 @@ def find_prime_slowly(number: int) -> int:
 
 @app.task(name="tasks.dummy_task")
 def dummy_task(name: str) -> str:
+    """
+    --- Documentación por ChatGPT ---
+    Tarea de Celery simulada (dummy) que imprime mensajes y espera un tiempo aleatorio antes de finalizar.
+
+    Params
+    ----------
+    :param name: str
+        Nombre identificador para la tarea, que se incluirá en los mensajes impresos.
+
+    :return: str
+    -------
+        Mensaje indicando la finalización de la tarea.
+
+    Notas
+    -----
+    Esta función imprime un mensaje al inicio y al final de su ejecución y
+    espera un tiempo aleatorio (entre 0 y 3 segundos) antes de finalizar.
+    Es útil para simular una tarea sin realizar ningún trabajo real.
+
+    Ejemplo
+    -------
+    >>> result = dummy_task.delay("TestTask")
+    >>> result.get(timeout=10)
+    "I'm Done"
+    """
     print(f"Dummy task starting: {name}")
     sleep(random() * 3)
     print(f"Dummy task executed: {name}")
@@ -68,6 +144,43 @@ def linear_regression(job_id: str,
                       amount_bought: int,
                       company_symbol: str,
                       starting_date_iso8601: str):
+    """
+    --- Documentación por ChatGPT ---
+    Predice el precio futuro de una acción basado en los datos históricos
+    y ajustado por la cantidad comprada.
+
+    Params
+    ----------
+    :param job_id: str
+        Identificador único para el trabajo en curso.
+    :param amount_bought: int
+        Cantidad de acciones compradas.
+    :param company_symbol: str
+        Símbolo de la compañía de la acción.
+    :param starting_date_iso8601: str
+        Fecha inicial en formato ISO 8601 desde la cual se comenzará a recopilar
+        los datos.
+
+    :return: float
+    -------
+        Precio esperado de la acción para el tiempo futuro calculado.
+
+    Notas
+    -----
+    Esta función primero recopila datos históricos de la acción desde una API
+    externa. Una vez recopilados los datos hasta la fecha de inicio
+    proporcionada, se utiliza una función de regresión lineal para predecir
+    el precio de la acción para una fecha futura. El tiempo futuro se calcula
+    como el tiempo actual más la diferencia entre el tiempo actual y el tiempo
+    de inicio. Finalmente, el precio previsto se ajusta por la cantidad de
+    acciones compradas usando `math_operations.expected_stock_price_with_adjustment`.
+
+    Ejemplo
+    -------
+    >>> result = linear_regression.delay("job123", 50, "AAPL", "2023-01-01T00:00:00Z")
+    >>> result.get(timeout=600)
+    123.45  # Este es solo un valor ilustrativo, no es el resultado real.
+    """
 
     # Declaro y defino variables
 
