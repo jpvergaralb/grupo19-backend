c3ba1f5aa4acd752240b5e5dd66fe9a29ce40f0f
Luis González <ljgonzalez@uc.cl>
2023-10-24 21:44:23 -0300

    Tests actualizados para nuevo json

--- workers/src/test_7_complete.py
+++ workers/src/test_7_complete.py
@@ -1,15 +1,10 @@
-import pytest
-from httpx import AsyncClient
-from app import app
-from tasks import app as celery_app
-from logs import logger as log
-from logs import print
-from environment import env
-from time import sleep
-import pytest
-from httpx import AsyncClient
 import asyncio
 import uuid
+
+import pytest
+from httpx import AsyncClient
+
+from app import app
 
 
 @pytest.mark.asyncio
@@ -59,11 +54,32 @@ async def test_jobs_pending():
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get(f"/job/{test_data['jobId']}")
     assert response.status_code == 200
-    assert response.json() == {
-        "job_id": test_data['jobId'],
-        "stocks_predictions": -2147483648,
-        "status": "PENDING"
-    }
+    response_data = response.json()
+
+    assert response_data["message"] == "Job found"
+    assert response_data["status"] == "PENDING"
+    assert response_data["job_id"] == test_data["jobId"]
+
+    job_data = response_data["job_data"]
+    assert job_data["stocks_predictions"] == 0
+    assert job_data["amount_bought"] >= 0
+    assert isinstance(job_data["amount_bought"], int)
+    assert job_data["company_symbol"] == test_data["symbol"]
+
+    job_times = job_data["times"]
+    assert job_times["prediction_starting_time"] == test_data["startingDate"]
+    assert job_times["run_at"] != "1970-01-01T00:00:00Z"
+    assert isinstance(job_times["run_at"], str)
+    assert job_times["prediction_future_time"] != "1970-01-01T00:00:00Z"
+    assert isinstance(job_times["prediction_future_time"], str)
+    assert job_times["delta_time_seconds"] > 0
+    assert isinstance(job_times["delta_time_seconds"], int)
+
+    for time_stamp, price in job_data["price_history"]:
+        assert time_stamp != "1970-01-01T00:00:00Z"
+        assert isinstance(time_stamp, str)
+        assert price not in (0, -2147483648)
+        assert isinstance(price, float)
 
 
 @pytest.mark.asyncio
@@ -77,11 +93,24 @@ async def test_jobs_not_found():
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get(f"/job/{test_data['jobId']}")
     assert response.status_code == 404
-    assert response.json() == {
-        "job_id": test_data['jobId'],
-        "stocks_predictions": -2147483648,
-        "message": "Job not found"
-    }
+    response_data = response.json()
+
+    assert response_data["message"] == "Job not found"
+    assert response_data["status"] == "UNKNOWN"
+    assert response_data["job_id"] == random_uuid
+
+    job_data = response_data["job_data"]
+    assert job_data["stocks_predictions"] == -2147483648
+    assert job_data["amount_bought"] == -2147483648
+    assert job_data["company_symbol"] == "UNKNOWN"
+
+    job_times = job_data["times"]
+    assert job_times["prediction_starting_time"] == "1970-01-01T00:00:00Z"
+    assert job_times["run_at"] == "1970-01-01T00:00:00Z"
+    assert job_times["prediction_future_time"] == "1970-01-01T00:00:00Z"
+    assert job_times["delta_time_seconds"] == 0
+
+    assert job_data["price_history"] == ["1970-01-01T00:00:00Z", -2147483648]
 
 
 @pytest.mark.asyncio
@@ -111,10 +140,24 @@ async def test_jobs_success():
     assert response.status_code == 200
     response_data = response.json()
 
-    assert isinstance(response_data["job_id"], str)
-    assert response_data["job_id"] == test_data["jobId"]
+    assert response_data["message"] == "Job found"
     assert response_data["status"] == "SUCCESS"
-    assert isinstance(response_data["stocks_predictions"], float)
+    assert response_data["job_id"] == test_data["jobId"]
+
+    job_data = response_data["job_data"]
+    assert job_data["stocks_predictions"] not in (0, -2147483648)
+    assert isinstance(job_data["stocks_predictions"], float)
+    assert job_data["amount_bought"] == job_data["amountValidated"]
+    assert isinstance(job_data["amount_bought"], int)
+    assert job_data["company_symbol"] == test_data["symbol"]
+
+    job_times = job_data["times"]
+    assert job_times["prediction_starting_time"] == test_data["startingDate"]
+    assert job_times["run_at"] == "1970-01-01T00:00:00Z"
+    assert job_times["prediction_future_time"] == "1970-01-01T00:00:00Z"
+    assert job_times["delta_time_seconds"] == 0
+
+    assert job_data["price_history"] == ["1970-01-01T00:00:00Z", 0]
 
 
 @pytest.mark.asyncio
