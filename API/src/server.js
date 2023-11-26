b69c057f097394e9e4f876e8b09b79927693fda3
Luis González <ljgonzalez@uc.cl>
2023-11-26 04:14:40 -0300

    🧱 infra(server.js): Añadir módulos y configuración de servidor WebSocket.

--- API/src/server.js
+++ API/src/server.js
@@ -1,6 +1,28 @@
+/* ------------------------------------------------------ */
+// Añadir módulos de websocket
+const { WebSocketServer } = require('ws');
+const http = require('http');
+/* ------------------------------------------------------ */
+
 const app = require('./app');
 
 const port = process.env.API_PORT || 8000;
+
+/* ------------------------------------------------------ */
+// Spinning the HTTP server and the WebSocket server.
+// Crear el servidor HTTP con la aplicación Express
+const server = http.createServer(app);
+// Configurar el servidor WebSocket para utilizar el mismo servidor HTTP
+const wsServer = new WebSocketServer({ server });
+
+
+wsServer.on('connection', (ws) => {
+  // Manejar los eventos de WebSocket
+  console.log('Cliente WebSocket conectado');
+  // --> AQUÍ <--
+});
+
+/* ------------------------------------------------------ */
 
 // eslint-disable-next-line
 process.env.NODE_ENV === 'test' ? null : app.listen(port, () => {
