ebcc05f1785b3a3312b148bb2b98e76f2605bae8
Luis González <ljgonzalez@uc.cl>
2023-11-26 12:56:37 -0300

    ✨ feat(API/config/adminList): Añadir nuevos correos a la lista blanca de administradores.

--- API/config/adminList.js
+++ API/config/adminList.js
@@ -1,3 +1,11 @@
-const allowedAdminsEmails = ['juanpytf2@gmail.com', 'beln.silva@uc.cl', 'francisco.campos@uc.cl', 'charlie.brown@example.com'];
+// email whitelist
+const allowedAdminsEmails = [
+  'juanpytf2@gmail.com',
+  'beln.silva@uc.cl',
+  'francisco.campos@uc.cl',
+  'charlie.brown@example.com',
+  'luis.alejandro1996@gmail.com',
+  'lj96gonzalez@gmail.com',
+];
 
 module.exports = allowedAdminsEmails;
